846a2953b528110785e533a3d401ab2c51900e02
Samaritan <nv5871nvs@gmail.com>
2020-04-01 22:40:02 +0800

    add index and bootstrap

--- src/main/java/com/community/controller/IndexController.java
+++ src/main/java/com/community/controller/IndexController.java
@@ -3,13 +3,14 @@ package com.community.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class HelloController {
+@Controller
+public class IndexController {
 
-    @RequestMapping("/hello")
-    public String hello() {
-        return "hello";
+    @RequestMapping("/")
+    public String index() {
+        return "index";
     }
 }
